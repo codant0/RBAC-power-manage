0bb723099f17b2400b4dbdf0c013a7c68339775c
code-zebra <879128177@qq.com>
2020-07-31 16:50:42 +0800

    Vue-RBAC前端

---
+++ rbac-front/src/App.vue
@@ -0,0 +1,44 @@
+<template>
+  <div id="app">
+    <Manage></Manage>
+    <div id="nav">
+      <router-link to="/">Login</router-link> |
+      <router-link to="/about">About</router-link> |
+      <router-link to="/index">Index</router-link>
+    </div>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+  import Manage from "./components/Manage";
+
+  export default {
+    components: {
+      Manage
+    }
+  }
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
+}
+</style>
--- rbac-front/src/views/Login.vue
+++ rbac-front/src/views/Login.vue
@@ -1,7 +1,7 @@
 <template>
 
   <el-main>
-    <el-form class="login-page" action="/login" method="post">
+    <el-form class="login-page" action="/test" method="post">
       <div class="form">
         <h3>账户登录</h3>
         <el-col :span="4" :push="10">
@@ -11,9 +11,16 @@
           <el-input placeholder="请输入密码" id="password" name="password"
                     required="required" v-model="password" show-password></el-input>
           <br/> <br/>
-          <el-button id="loginBtn" type="primary" round>登录</el-button>
+          <el-button id="loginBtn" type="primary" @click="login" round>登录</el-button>
         </el-col>
-
+        <br/>
+        <br/>
+        <br/>
+        <br/>
+        <br/>
+        <br/>
+<!--        <div>{{username}}</div>-->
+<!--        <div>{{password}}</div>-->
       </div>
     </el-form>
   </el-main>
@@ -28,6 +35,31 @@ export default {
       message: 'This is Login Message',
       username: '',
       password: ''
+    }
+  },
+  methods: {
+    login() {
+      console.log("login");
+      this.$axios({
+        method: "post",
+        url: "http://localhost:8081/test",
+        data: {
+          username: this.username,
+          password: this.password
+        },
+        crossDomain: true,
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded',
+        }
+      })
+      .then(response => {
+        console.log(response, "success");
+        alert("success");
+      })
+      .catch(error => {
+        console.log(error, "error");
+        alert("fail")
+      })
     }
   }
 }
